4b39419aca65ef959c3feda02ca142cd060f509d
Piotr Krzaczkowski <slimshade93@wp.pl>
2021-01-21 20:33:43 +0100

    added bmi calculator logic

--- index.js
+++ index.js
@@ -54,6 +54,30 @@ const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 700,
 });
 
-window.onbeforeunload = () => {
-  window.scrollTo(0, 1400);
-};
+// CALCULATOR
+
+const height = document.getElementById("height").value;
+const weight = document.getElementById("weight").value;
+let bmi = weight / (((height / 100) * height) / 100);
+let bmiOutput = bmi.toFixed(2);
+
+const calcButton = document.getElementById("bmiBtn");
+
+calcButton.addEventListener("click", () => {
+  const height = document.getElementById("height").value;
+  const weight = document.getElementById("weight").value;
+  let bmi = weight / (((height / 100) * height) / 100);
+  let bmiOutput = bmi.toFixed(2);
+  document.getElementById("result").innerHTML = `Your BMI is ${bmiOutput}`;
+  setTimeout(() => {
+    document.getElementById("result").innerHTML = "result";
+  }, 5000);
+});
+
+document.querySelector(".mobile-link.bmi").addEventListener("click", () => {
+  document.querySelector(".calculator").classList.add("active");
+});
+
+document.querySelector(".toggle-calc").addEventListener("click", () => {
+  document.querySelector(".calculator").classList.remove("active");
+});
